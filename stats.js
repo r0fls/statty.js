8803d69cd58fb839ad1e143087b028a74d308876
r0fls <raphael.deem@gmail.com>
2016-01-08 22:23:12 -0800

    mean, variance refactor

--- stats.js
+++ stats.js
@@ -53,88 +53,97 @@ function normal(mean,variance) {
 }
 
 normal.fit = function(data){
-  mean = 0;
-  for (i=0;i<data.length;i++){
-    mean += data[i]
-  }
-  mean = mean/data.length;
+  mean = get_mean(data);
+  variance = get_variance(data,mean);
+  return normal(mean,variance)
+}
+
+function get_variance(data,mean){
   variance = 0;
   for (j=0;j<data.length;j++){
     variance += Math.pow(mean-data[j],2);
   }
-  variance = variance/(data.length-1);
-  return normal(mean,variance)
+  return variance/(data.length-1);
+
 }
 
-
-function erf_series(x,n){
+function get_mean(data){
   total = 0;
-  for (k=0;k<n;k++){
-    total += erf_term(x,k);
+  for (i=0;i<data.length;i++){
+    total += data[i]
   }
-  return total;
+  return  total/data.length
 }
 
-function erf_term(x,k){
-  return 2*Math.pow(-1,k)*Math.pow(x,1+2*k)/((1+2*k)*factorial(k)*Math.sqrt(Math.PI))
-}
 
-var f = [];
-function factorial (n) {
-  if (n == 0 || n == 1)
-    return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
-}
-
-function inverse_erf(x){
-  a = 8*(Math.PI - 3)/(3*Math.PI*(4-Math.PI))
-  term1 = Math.pow((2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2,2) - Math.log(1-Math.pow(x,2))/a
-  term2 = (2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2
-  return (Math.abs(x)/x)*Math.sqrt(Math.sqrt(term1)-term2) 
-}
-
-function uniform(a,b){
-  a = typeof a !== 'undefined' ? a : 0;
-  b = typeof b !== 'undefined' ? b : 1;
-  this.a = a;
-  this.b = b;
-  this.mean = (this.a - this.b)/2;
-  this.variance = Math.pow(this.b - this.a,2)/12
-
-  this.pdf = function(x){
-    if (x>a && x<b){ 
-      return 1/(this.b-this.a)
+  function erf_series(x,n){
+    total = 0;
+    for (k=0;k<n;k++){
+      total += erf_term(x,k);
     }
-    return 0
+    return total;
   }
 
-  this.cdf = function(x){
-    if (x<this.a){
+  function erf_term(x,k){
+    return 2*Math.pow(-1,k)*Math.pow(x,1+2*k)/((1+2*k)*factorial(k)*Math.sqrt(Math.PI))
+  }
+
+  var f = [];
+  function factorial (n) {
+    if (n == 0 || n == 1)
+      return 1;
+    if (f[n] > 0)
+      return f[n];
+    return f[n] = factorial(n-1) * n;
+  }
+
+  function inverse_erf(x){
+    a = 8*(Math.PI - 3)/(3*Math.PI*(4-Math.PI))
+    term1 = Math.pow((2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2,2) - Math.log(1-Math.pow(x,2))/a
+    term2 = (2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2
+    return (Math.abs(x)/x)*Math.sqrt(Math.sqrt(term1)-term2) 
+  }
+
+  function uniform(a,b){
+    a = typeof a !== 'undefined' ? a : 0;
+    b = typeof b !== 'undefined' ? b : 1;
+    this.a = a;
+    this.b = b;
+    this.mean = (this.a - this.b)/2;
+    this.variance = Math.pow(this.b - this.a,2)/12
+
+    this.pdf = function(x){
+      if (x>a && x<b){ 
+        return 1/(this.b-this.a)
+      }
       return 0
     }
-    if (x<this.b){
-      return (x-this.a)/(this.b-this.a)
-    }
-    if (x>=this.b){
-      return 1
-    }
-  }
 
-  this.quantile = function(x){
-    return x*(this.b-this.a)+this.a
-  }
-
-  this.rand = function(n){
-    if (n>0){
-      arr = new Array;
-      for (i=0;i++;i<n){
-        arr.push(this.a + (this.b - this.a)*Math.random());
+    this.cdf = function(x){
+      if (x<this.a){
+        return 0
       }
-      return arr;
+      if (x<this.b){
+        return (x-this.a)/(this.b-this.a)
+      }
+      if (x>=this.b){
+        return 1
+      }
     }
+
+    this.quantile = function(x){
+      return x*(this.b-this.a)+this.a
+    }
+
+    this.rand = function(n){
+      if (n>0){
+        arr = new Array;
+        for (i=0;i++;i<n){
+          arr.push(this.a + (this.b - this.a)*Math.random());
+        }
+        return arr;
+      }
       return this.a + (this.b - this.a)*Math.random()
+    }
+    return this
   }
-  return this
-}

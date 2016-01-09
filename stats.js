95e8790b8ac03bdf488faf52a7d589f63d54637a
r0fls <raphael.deem@gmail.com>
2016-01-08 23:03:23 -0800

    uniform fit, random array, mean calculation

--- stats.js
+++ stats.js
@@ -76,74 +76,92 @@ function get_mean(data){
 }
 
 
-  function erf_series(x,n){
-    total = 0;
-    for (k=0;k<n;k++){
-      total += erf_term(x,k);
+function erf_series(x,n){
+  total = 0;
+  for (k=0;k<n;k++){
+    total += erf_term(x,k);
+  }
+  return total;
+}
+
+function erf_term(x,k){
+  return 2*Math.pow(-1,k)*Math.pow(x,1+2*k)/((1+2*k)*factorial(k)*Math.sqrt(Math.PI))
+}
+
+var f = [];
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+}
+
+function inverse_erf(x){
+  a = 8*(Math.PI - 3)/(3*Math.PI*(4-Math.PI))
+  term1 = Math.pow((2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2,2) - Math.log(1-Math.pow(x,2))/a
+  term2 = (2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2
+  return (Math.abs(x)/x)*Math.sqrt(Math.sqrt(term1)-term2) 
+}
+
+function uniform(a,b){
+  a = typeof a !== 'undefined' ? a : 0;
+  b = typeof b !== 'undefined' ? b : 1;
+  this.a = a;
+  this.b = b;
+  this.mean = (this.a + this.b)/2;
+  this.variance = Math.pow(this.b - this.a,2)/12
+
+  this.pdf = function(x){
+    if (x>a && x<b){ 
+      return 1/(this.b-this.a)
     }
-    return total;
+    return 0
   }
 
-  function erf_term(x,k){
-    return 2*Math.pow(-1,k)*Math.pow(x,1+2*k)/((1+2*k)*factorial(k)*Math.sqrt(Math.PI))
-  }
-
-  var f = [];
-  function factorial (n) {
-    if (n == 0 || n == 1)
-      return 1;
-    if (f[n] > 0)
-      return f[n];
-    return f[n] = factorial(n-1) * n;
-  }
-
-  function inverse_erf(x){
-    a = 8*(Math.PI - 3)/(3*Math.PI*(4-Math.PI))
-    term1 = Math.pow((2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2,2) - Math.log(1-Math.pow(x,2))/a
-    term2 = (2/(Math.PI*a)) + Math.log(1-Math.pow(x,2))/2
-    return (Math.abs(x)/x)*Math.sqrt(Math.sqrt(term1)-term2) 
-  }
-
-  function uniform(a,b){
-    a = typeof a !== 'undefined' ? a : 0;
-    b = typeof b !== 'undefined' ? b : 1;
-    this.a = a;
-    this.b = b;
-    this.mean = (this.a - this.b)/2;
-    this.variance = Math.pow(this.b - this.a,2)/12
-
-    this.pdf = function(x){
-      if (x>a && x<b){ 
-        return 1/(this.b-this.a)
-      }
+  this.cdf = function(x){
+    if (x<this.a){
       return 0
     }
-
-    this.cdf = function(x){
-      if (x<this.a){
-        return 0
-      }
-      if (x<this.b){
-        return (x-this.a)/(this.b-this.a)
-      }
-      if (x>=this.b){
-        return 1
-      }
+    if (x<this.b){
+      return (x-this.a)/(this.b-this.a)
     }
-
-    this.quantile = function(x){
-      return x*(this.b-this.a)+this.a
+    if (x>=this.b){
+      return 1
     }
-
-    this.rand = function(n){
-      if (n>0){
-        arr = new Array;
-        for (i=0;i++;i<n){
-          arr.push(this.a + (this.b - this.a)*Math.random());
-        }
-        return arr;
-      }
-      return this.a + (this.b - this.a)*Math.random()
-    }
-    return this
   }
+
+  this.quantile = function(x){
+    return x*(this.b-this.a)+this.a
+  }
+
+  this.rand = function(n){
+    n = typeof n !== 'undefined' ? n : 1;
+    if (n>1){
+      arr = new Array;
+      for (i=0;i<n;i++){
+        arr.push(this.a + (this.b - this.a)*Math.random());
+      }
+      return arr;
+    }
+    return this.a + (this.b - this.a)*Math.random()
+  }
+  return this
+}
+
+uniform.fit = function(data){
+  midrange = (max(data)+min(data))/2;
+  range = max(data)-min(data);
+  a = midrange - (range/2);
+  b = midrange + (range/2);
+  return uniform(a,b)
+}
+function max(data){
+  return Math.max.apply(null, data);
+}
+
+function min(data){
+  return Math.min.apply(null, data);
+}
+
+
